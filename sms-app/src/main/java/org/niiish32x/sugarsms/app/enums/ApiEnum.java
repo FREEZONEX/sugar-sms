1922eacb4c5e523568d327db0253c77c21e0486b
nishenghao <1159417019@qq.com>
2024-12-08 18:24:28 +0800

    优化 统一API

---
+++ sms-app/src/main/java/org/niiish32x/sugarsms/app/enums/ApiEnum.java
@@ -0,0 +1,16 @@
+package org.niiish32x.sugarsms.app.enums;
+
+/**
+ * ApiEnum
+ *
+ * @author shenghao ni
+ * @date 2024.12.08 18:22
+ */
+public enum ApiEnum {
+
+    USER_API("/open-api/auth/v2/users");
+
+    ApiEnum(String value) {
+
+    }
+}
