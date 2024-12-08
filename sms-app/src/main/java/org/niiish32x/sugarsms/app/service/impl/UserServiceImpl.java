5f96543bf48b806bd5a5b9d4a5a1dfd4c8c26ccc
nishenghao <1159417019@qq.com>
2024-12-08 20:07:35 +0800

    fix

--- sms-app/src/main/java/org/niiish32x/sugarsms/app/service/impl/UserServiceImpl.java
+++ sms-app/src/main/java/org/niiish32x/sugarsms/app/service/impl/UserServiceImpl.java
@@ -5,10 +5,9 @@ import cn.hutool.http.HttpResponse;
 import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.annotation.JSONField;
 import lombok.Data;
-import org.niiish32x.sugarsms.app.dto.PageDTO;
 import org.niiish32x.sugarsms.app.dto.SuposUserDTO;
 import org.niiish32x.sugarsms.app.enums.ApiEnum;
-import org.niiish32x.sugarsms.app.service.SuposUserService;
+import org.niiish32x.sugarsms.app.service.UserService;
 import org.niiish32x.sugarsms.common.supos.request.PageResponse;
 import org.niiish32x.sugarsms.common.supos.request.SuposRequestManager;
 import org.springframework.stereotype.Service;
@@ -26,7 +25,7 @@ import java.util.Map;
  * @date 2024.12.08 13:34
  */
 @Service
-public class SuposUserServiceImpl implements SuposUserService {
+public class UserServiceImpl implements UserService {
 
     @Resource
     SuposRequestManager suposRequestManager;
