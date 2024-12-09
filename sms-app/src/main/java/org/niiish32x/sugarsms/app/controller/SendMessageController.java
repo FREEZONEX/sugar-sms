a7dcc3f0e462017d6b4b9ca897f5ea4e43411c1f
nishenghao <1159417019@qq.com>
2024-12-09 10:11:33 +0800

    SendMessage

---
+++ sms-app/src/main/java/org/niiish32x/sugarsms/app/controller/SendMessageController.java
@@ -0,0 +1,25 @@
+package org.niiish32x.sugarsms.app.controller;
+
+import org.niiish32x.sugarsms.app.service.SendMessageService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * SendMessageController
+ *
+ * @author shenghao ni
+ * @date 2024.12.09 9:43
+ */
+
+@RestController
+public class SendMessageController {
+    @Resource
+    SendMessageService sendMessageService;
+
+    @RequestMapping("/send/test")
+    public void test(){
+        sendMessageService.sendOne();
+    }
+}
---
+++ sms-app/src/main/java/org/niiish32x/sugarsms/app/service/SendMessageService.java
@@ -0,0 +1,11 @@
+package org.niiish32x.sugarsms.app.service;
+
+/**
+ * SendMessageService
+ *
+ * @author shenghao ni
+ * @date 2024.12.09 9:42
+ */
+public interface SendMessageService {
+    void sendOne();
+}
