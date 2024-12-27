ed3bb7ab231ab88690d646e8b747e85a4407974b
nishenghao <1159417019@qq.com>
2024-12-27 11:22:15 +0800

    feat: 优化邮件发送方法 及其配置

--- sms-app/src/main/java/org/niiish32x/sugarsms/app/proxy/EmailSenderProxy.java
+++ sms-app/src/main/java/org/niiish32x/sugarsms/app/proxy/EmailSenderProxy.java
@@ -1,13 +1,13 @@
 package org.niiish32x.sugarsms.app.proxy;
 
+import com.google.common.util.concurrent.RateLimiter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.AddressException;
+import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.util.Properties;
@@ -21,9 +21,25 @@ import java.util.Properties;
 
 
 @Slf4j
+@Component
 public class EmailSenderProxy {
 
+
+    @Value("${mail.smtp.host}")
+    private String SMTP_HOST ;
+    @Value("${mail.smtp.port}")
+    private String SMTP_PORT ;
+    @Value("${mail.smtp.auth}")
+    private  String SMTP_AUTH ;
+    @Value("${mail.smtp.password}")
+    private  String SMTP_PASSWORD ;
+    @Value("${mail.smtp.starttls.enable}")
+    private  String SMTP_STARTTLS_ENABLE;
+
     public static final String PUBLIC_EMAIL_SUGAR = "techalert@dhampursugar.com";
+
+    // ratelimiter 限流 smtp 有并发数限制
+    private static final RateLimiter limiter = RateLimiter.create(10);
 
 
     /**
@@ -46,9 +62,9 @@ public class EmailSenderProxy {
         properties.put("mail.smtp.starttls.enable", "true");
 
         // 获取邮件会话对象
-        Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
-            protected javax.mail.PasswordAuthentication getPasswordAuthentication() {
-                return new javax.mail.PasswordAuthentication(from, password);
+        Session session = Session.getInstance(properties, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(from, password);
             }
         });
 
@@ -73,23 +89,80 @@ public class EmailSenderProxy {
         }
     }
 
-    public static boolean sendTextEmail(String from, String to, String subject, String content) {
+//    public static boolean sendTextEmail(String from, String to, String subject, String content) {
+//
+//        String password = "L*112832568463od";
+//
+//        // 设置邮件服务器相关属性
+//        Properties properties = new Properties();
+//        // 以QQ邮箱的SMTP服务器为例
+//        properties.put("mail.smtp.host", "smtp.office365.com");
+//        properties.put("mail.smtp.port", "25");
+//        properties.put("mail.smtp.auth", "true");
+//        properties.put("mail.smtp.password","L*112832568463od");
+//        properties.put("mail.smtp.starttls.enable", "true");
+//
+//        // 获取邮件会话对象
+//        Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
+//            protected javax.mail.PasswordAuthentication getPasswordAuthentication() {
+//                return new javax.mail.PasswordAuthentication(from, password);
+//            }
+//        });
+//
+//        try {
+//            // 创建邮件消息对象
+//            MimeMessage message = new MimeMessage(session);
+//            // 设置发件人
+//            message.setFrom(new InternetAddress(from));
+//            // 设置收件人
+//            message.setRecipient(Message.RecipientType.TO, new InternetAddress(to));
+//            // 设置邮件主题
+//            message.setSubject(subject);
+//            // 设置邮件正文内容
+//            message.setText(content);
+//
+//            // 发送邮件
+//            Transport.send(message);
+//            log.info("邮件发送成功! 通知邮箱 {}",to);
+//
+//            return true;
+//        } catch (MessagingException e) {
+//            e.printStackTrace();
+//            log.error("{} 邮件发送失败 {}",to,e.getMessage());
+//            return false;
+//        }
+//    }
 
-        String password = "L*112832568463od";
+    public boolean sendTextEmail(String from, String to, String subject, String content) {
+
+
+        // 验证输入参数
+        if (from == null || to == null || subject == null || content == null) {
+            log.error("输入参数不能为空");
+            return false;
+        }
+
+        limiter.acquire();
 
         // 设置邮件服务器相关属性
         Properties properties = new Properties();
-        // 以QQ邮箱的SMTP服务器为例
-        properties.put("mail.smtp.host", "smtp.office365.com");
-        properties.put("mail.smtp.port", "25");
-        properties.put("mail.smtp.auth", "true");
-        properties.put("mail.smtp.password","L*112832568463od");
-        properties.put("mail.smtp.starttls.enable", "true");
+
+
+        if (isValid(SMTP_HOST) && isValid(SMTP_PORT) && isValid(SMTP_AUTH) && isValid(SMTP_STARTTLS_ENABLE)) {
+            properties.put("mail.smtp.host", SMTP_HOST);
+            properties.put("mail.smtp.port", SMTP_PORT);
+            properties.put("mail.smtp.auth", SMTP_AUTH);
+            properties.put("mail.smtp.starttls.enable", SMTP_STARTTLS_ENABLE);
+        } else {
+            throw new IllegalArgumentException("Invalid SMTP configuration parameters");
+        }
+
+
 
         // 获取邮件会话对象
-        Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
-            protected javax.mail.PasswordAuthentication getPasswordAuthentication() {
-                return new javax.mail.PasswordAuthentication(from, password);
+        Session session = Session.getInstance(properties, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(from, SMTP_PASSWORD);
             }
         });
 
@@ -107,13 +180,18 @@ public class EmailSenderProxy {
 
             // 发送邮件
             Transport.send(message);
-            log.info("邮件发送成功! 通知邮箱 {}",to);
+            log.info("邮件发送成功! 通知邮箱 {}", to);
 
             return true;
         } catch (MessagingException e) {
-            e.printStackTrace();
-            log.error("{} 邮件发送失败 {}",to,e.getMessage());
+            log.error("邮件发送失败: {}, 错误信息: {}", to, e.getMessage());
             return false;
         }
     }
+
+
+    private static boolean isValid(String value) {
+        return value != null && !value.trim().isEmpty();
+    }
+
 }
--- sms-app/src/main/java/org/niiish32x/sugarsms/app/service/impl/SendMessageImpl.java
+++ sms-app/src/main/java/org/niiish32x/sugarsms/app/service/impl/SendMessageImpl.java
@@ -1,6 +1,7 @@
 package org.niiish32x.sugarsms.app.service.impl;
 
 import com.alibaba.fastjson2.JSON;
+import com.google.common.util.concurrent.RateLimiter;
 import lombok.extern.slf4j.Slf4j;
 import org.niiish32x.sugarsms.app.external.ZubrixSmsResponse;
 import org.niiish32x.sugarsms.app.proxy.EmailSenderProxy;
@@ -8,6 +9,7 @@ import org.niiish32x.sugarsms.app.proxy.ZubrixSmsProxy;
 import org.niiish32x.sugarsms.app.service.SendMessageService;
 import org.niiish32x.sugarsms.common.result.Result;
 import org.niiish32x.sugarsms.common.result.ResultCode;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -28,6 +30,11 @@ public class SendMessageImpl implements SendMessageService {
     ZubrixSmsProxy zubrixSmsProxy;
 
 
+    @Autowired
+    EmailSenderProxy emailSenderProxy;
+
+    private final RateLimiter rateLimiter = RateLimiter.create(10);
+
     @Override
     public Result <ZubrixSmsResponse > sendOneZubrixSmsMessage(String number, String text) {
         ZubrixSmsResponse messageResponse = zubrixSmsProxy.send(number,text);
@@ -38,12 +45,17 @@ public class SendMessageImpl implements SendMessageService {
 
     @Override
     public boolean sendEmail() {
-        return EmailSenderProxy.sendTextEmail(EmailSenderProxy.PUBLIC_EMAIL_SUGAR,"niiish32x@gmail.com","test","测试");
+        return emailSenderProxy.sendTextEmail(EmailSenderProxy.PUBLIC_EMAIL_SUGAR,"1159417019@qq.com","test","测试");
     }
 
     @Override
     public boolean sendEmail(String toMail, String subject, String text) {
-        return EmailSenderProxy.sendTextEmail(EmailSenderProxy.PUBLIC_EMAIL_SUGAR,toMail,subject,text);
+        if (rateLimiter.tryAcquire()) {
+            return emailSenderProxy.sendTextEmail(EmailSenderProxy.PUBLIC_EMAIL_SUGAR,toMail,subject,text);
+        }else {
+            return false;
+        }
+
     }
 
 
