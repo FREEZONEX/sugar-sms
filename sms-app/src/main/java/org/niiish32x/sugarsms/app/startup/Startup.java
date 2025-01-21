474de548a82c2595fe7dd3766b958548d4dcb953
nishenghao <1159417019@qq.com>
2025-01-21 16:46:17 +0800

    feat: 自动消息确认

--- sms-app/src/main/java/org/niiish32x/sugarsms/app/startup/Startup.java
+++ sms-app/src/main/java/org/niiish32x/sugarsms/app/startup/Startup.java
@@ -1,5 +1,6 @@
 package org.niiish32x.sugarsms.app.startup;
 
+import lombok.extern.slf4j.Slf4j;
 import org.niiish32x.sugarsms.alert.app.AlertService;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Component;
  * @date 2025.01.21 16:41
  */
 
+@Slf4j
 @Component
 public class Startup implements InitializingBean {
 
@@ -20,7 +22,9 @@ public class Startup implements InitializingBean {
 
     @Override
     public void afterPropertiesSet() throws Exception {
+        log.info("start ack all old alerts");
         // 确认所有消息 防止重启后一次性发送过多
         alertService.ackAlerts();
+        log.info("finish ack all old alerts");
     }
 }
